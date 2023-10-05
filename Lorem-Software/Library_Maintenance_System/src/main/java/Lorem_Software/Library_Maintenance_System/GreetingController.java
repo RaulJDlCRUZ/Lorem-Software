91ec3ac30c171617ab01c43819b3b80fb75af85a
RaulJDlCRUZ <114936538+RaulJDlCRUZ@users.noreply.github.com>
2023-10-05 17:28:08 +0200

    This commit pretends to upload the entire project to the repo.

---
+++ Lorem-Software/Library_Maintenance_System/src/main/java/Lorem_Software/Library_Maintenance_System/GreetingController.java
@@ -0,0 +1,24 @@
+package Lorem_Software.Library_Maintenance_System;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import Lorem_Software.Library_Maintenance_System.entity.Greeting;
+
+@Controller
+public class GreetingController {
+	@GetMapping("/greeting")
+	public String greetingForm(Model model) {
+		model.addAttribute("greeting", new Greeting());
+		return "greeting";
+	}
+
+	@PostMapping("/greeting")
+	public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+		model.addAttribute("greeting", greeting);
+		return "result";
+	}
+}
